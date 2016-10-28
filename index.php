64afac7c1fba9ecb28924fe50c47c5b381d6884e
Dyan Galih <dyan.galih@gmail.com>
2016-10-28 21:39:29 +0700

    Clean up projet parameter

--- index.php
+++ index.php
@@ -6,16 +6,9 @@ error_reporting(E_ALL);
 /*
 default docroot aplikasi
  */
-$arrData = explode('/', $_SERVER["SCRIPT_NAME"]);
+$arrData     = explode('/', $_SERVER["SCRIPT_NAME"]);
 $replaceChar = "/" . $arrData[count($arrData) - 1];
-$docRoot = str_replace($replaceChar, "/", $_SERVER['SCRIPT_FILENAME']);
-
-/*
-hijack untuk kebutuhan tantraz
- */
-//$docRoot = "/home2/tantrazc/public_html/apps/";
-
-$docRoot = "/Users/dyangalih/webserv/galih/tantraz_clean/apps/";
+$docRoot     = str_replace($replaceChar, "/", $_SERVER['SCRIPT_FILENAME']);
 
 //require mainbase
 require_once 'sf/index.php';
